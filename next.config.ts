d990b3b5fc584434e3698f49671b76df575e59a6
HarryT <thcstrandainghiathinh6a1@gmail.com>
2026-03-27 12:50:09 +0700

    fix npm run build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    root: process.cwd(),
+  },
 
   async rewrites() {
     return [
